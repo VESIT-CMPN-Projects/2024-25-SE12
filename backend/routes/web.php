fe60c49a70d1d0386a49ee178ed22173cb778a64
riddhij0804 <riddhijangale0804@gmail.com>
2025-04-02 00:43:41 +0530

    ourteam.html to blade

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('homePage');
+});
+
 Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/', function () {
-    return view('homePage');
+Route::get('/team', function () {
+    return view('ourTeam');
 });
+
